4779d805050c886226cdb8b21a288955531ad972
ultimatile <ultimatile@users.noreply.github.com>
2025-07-13 07:21:32 +0900

    `/docs/reference/layout/layout`の翻訳

--- crates/typst-library/src/layout/layout.rs
+++ crates/typst-library/src/layout/layout.rs
@@ -9,14 +9,11 @@ use crate::foundations::{
 use crate::introspection::Locatable;
 use crate::layout::{BlockElem, Size};
 
-/// Provides access to the current outer container's (or page's, if none)
-/// dimensions (width and height).
+/// 外側のコンテナ（存在しなければページ）の寸法（幅と高さ）へのアクセスを提供します。
 ///
-/// Accepts a function that receives a single parameter, which is a dictionary
-/// with keys `width` and `height`, both of type [`length`]. The function is
-/// provided [context], meaning you don't need to use it in combination with the
-/// `context` keyword. This is why [`measure`] can be called in the example
-/// below.
+/// `width`と`height`という[`length`]型のキーを持つ辞書を単一の引数として受け取る関数を受け付けます。
+/// 関数には[context]が渡されるため、`context`キーワードと組み合わせて使用する必要はありません。
+/// これが以下の例で[`measure`]が呼び出せる理由です。
 ///
 /// ```example
 /// #let text = lorem(30)
@@ -30,18 +27,14 @@ use crate::layout::{BlockElem, Size};
 /// ])
 /// ```
 ///
-/// Note that the `layout` function forces its contents into a [block]-level
-/// container, so placement relative to the page or pagebreaks are not possible
-/// within it.
+/// `layout`関数はコンテンツに[ブロック]($block)レベルのコンテナであることを強制するため、その内部でページを基準とした配置やページ区切りはできないことを注意します。
 ///
-/// If the `layout` call is placed inside a box with a width of `{800pt}` and a
-/// height of `{400pt}`, then the specified function will be given the argument
-/// `{(width: 800pt, height: 400pt)}`. If it is placed directly into the page, it
-/// receives the page's dimensions minus its margins. This is mostly useful in
-/// combination with [measurement]($measure).
+/// 例えば、幅が`{800pt}`で高さが`{400pt}`のボックスの中から`layout`が呼び出されたとすると、指定された関数には`{(width: 800pt, height: 400pt)}`という引数が与えられます。
+/// ページに直接置かれた場合は、ページの寸法からそのマージンを引いたものを受け取ります。
+/// これは主に[測定]($measure)と組み合わせるときに便利です。
 ///
-/// You can also use this function to resolve [`ratio`] to fixed lengths. This
-/// might come in handy if you're building your own layout abstractions.
+/// この関数は、[ratio]を固定長に変換するためにも利用できます。
+/// これは独自のレイアウト抽象化を構築する際に重宝するかもしれません。
 ///
 /// ```example
 /// #layout(size => {
@@ -50,20 +43,17 @@ use crate::layout::{BlockElem, Size};
 /// })
 /// ```
 ///
-/// Note that the width or height provided by `layout` will be infinite if the
-/// corresponding page dimension is set to `{auto}`.
+/// `layout`が提供する幅と高さは、対象ページの寸法が`{auto}`に設定されていると無限大になりうることを注意します。
 #[func]
 pub fn layout(
     span: Span,
-    /// A function to call with the outer container's size. Its return value is
-    /// displayed in the document.
+    /// 外側のコンテナの大きさと一緒に呼び出す関数。
+    /// 戻り値は文書に表示されます。
     ///
-    /// The container's size is given as a [dictionary] with the keys `width`
-    /// and `height`.
+    /// コンテナの大きさは`width`と`height`のキーを持つ[dictionary]として与えられます。
     ///
-    /// This function is called once for each time the content returned by
-    /// `layout` appears in the document. This makes it possible to generate
-    /// content that depends on the dimensions of its container.
+    /// この関数は、`layout` によって返されるコンテンツが文書中に現れるたびに呼び出されます。
+    /// これによりそのコンテナの寸法に依存するコンテンツを生成することが可能になります。
     func: Func,
 ) -> Content {
     LayoutElem::new(func).pack().spanned(span)
